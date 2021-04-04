552160aa8db64ebe737d5a41ee2e64ef67b92b15
Candice <candylol17@gmail.com>
2021-04-04 23:54:52 +0200

    OpenAPI (swagger) configuration initialization

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CQRS library project - TDD course')
+    .setDescription('The CQRS library API description')
+    .setVersion('1.0')
+    .addTag('library')
+    .build();
+  const document = SwaggerModule.createDocument(app, config); //can be saved as JSON/YAML file 
+  SwaggerModule.setup('api', app, document);
+
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
