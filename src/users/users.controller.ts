e4a2cb39cef8cc945d6419930a2e50bbe639f2fa
Amine <amine1u1@outlook.com>
2021-04-06 19:55:58 +0200

    :construction: Update UsersController to use correct parameter names

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,42 +1,39 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users Details')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll(): Promise<User[]> {
+    return await this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @Get(':username')
+  async findOne(@Param('username') username: string): Promise<User> {
+    return await this.usersService.findByUsername(username);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
+  /* TODO : Update
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    @Patch(':username')
+    update(@Param('username') username: string, @Body() updateUserDto: UpdateUserDto) {
+      return this.usersService.update(username, updateUserDto);
+    }
+   */
+
+  @Delete(':username')
+  remove(@Param('username') username: string) {
+    return this.usersService.remove(username);
   }
 }
