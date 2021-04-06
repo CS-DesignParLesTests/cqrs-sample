55530aa41b9c2f799e128105aa7dd056b23f6310
Amine <amine1u1@outlook.com>
2021-04-06 20:21:07 +0200

    :adhesive_bandage: Imported the CqrsModule into the UsersController testing module

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { CqrsModule } from '@nestjs/cqrs';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CqrsModule],
       controllers: [UsersController],
       providers: [UsersService],
     }).compile();
