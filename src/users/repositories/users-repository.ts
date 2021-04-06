79af3552da08a24f0e6f2f9f3c1a0299e9b5ef61
Amine <amine1u1@outlook.com>
2021-04-06 19:41:47 +0200

    :building_construction: Create an abstract UserRepository. Its implementations will handle communicating with a User Storage

---
+++ src/users/repositories/users-repository.ts
@@ -0,0 +1,18 @@
+import { User } from '../entities/user.entity';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { Injectable } from '@nestjs/common';
+import { UpdateUserDto } from '../dto/update-user.dto';
+
+@Injectable()
+export abstract class UsersRepository {
+  abstract findOneByUsername(username: string): Promise<User>;
+
+  abstract findAll(): Promise<User[]>;
+
+  abstract create(payload: CreateUserDto): Promise<User>;
+
+  abstract update(payload: UpdateUserDto): Promise<User>;
+
+  abstract delete(username: string): Promise<void>;
+
+}
