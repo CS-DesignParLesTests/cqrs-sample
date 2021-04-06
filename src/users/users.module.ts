4a2254c3cc59bdedecf036bb714ae4e68398a840
Amine <amine1u1@outlook.com>
2021-04-06 19:52:32 +0200

    :building_construction: Create Query and Command Handlers list to be injected in the module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { QueryHandlers } from './queries/handlers';
+import { CommandHandlers } from './commands/handlers';
 
 @Module({
   imports: [CqrsModule],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, ...CommandHandlers, ...QueryHandlers],
 })
 export class UsersModule {}
