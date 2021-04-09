f322cadc8f9c13c35315afe356f727f84c6bac52
Amine <amine1u1@outlook.com>
2021-04-09 22:13:12 +0200

    :label: Updated the BookOwnership entity & constructor
    - Extend from CQRS AggregateRoot class
    - Properly handle all parameters from the constructor
    - In order for the TypeORM to use the constructor it needs to accept undefined parameter
    - Otherwise it needs username & bookId properties

--- src/book-ownerships/entities/book-ownership.entity.ts
+++ src/book-ownerships/entities/book-ownership.entity.ts
@@ -1,7 +1,8 @@
 import { Book } from '../../books/entities/book.entity';
+import { AggregateRoot } from '@nestjs/cqrs';
 
 //TODO Implement BookInterface to apply decorator pattern ?
-export class BookOwnership {
+export class BookOwnership extends AggregateRoot {
   public username: string;
   public bookId: string;
 
@@ -12,10 +13,24 @@ export class BookOwnership {
   public dateAcquired: Date | undefined;
   public dateRead: Date | undefined;
 
-  constructor({ username, bookId }) {
-    this.username = username;
+  constructor(input) {
+    // need to check if input is defined because TypeOrm call the constructor without parameters
+    super();
+    if (input === undefined) return;
     //TODO Store book instance instead ?
-    this.bookId = bookId;
+    ({
+      username: this.username,
+      bookId: this.bookId,
+      bought: this.bought,
+      signed: this.signed,
+      lent: this.lent,
+      dateAcquired: this.dateAcquired,
+      dateRead: this.dateRead,
+    } = input);
+
+    if (!this.username || !this.bookId) {
+      throw new Error('Missing username or bookId');
+    }
   }
 
   /*
