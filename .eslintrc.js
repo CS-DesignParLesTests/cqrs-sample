a6602b0a96872ee6416358134b8eea8a8ffb4c82
JeromePto <petot.jerome@gmail.com>
2021-04-09 18:23:57 +0200

    linter rule for comment space to warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'spaced-comment': ['error', 'always', { 'markers': ['!', '?', 'TODO'] }],
+    'spaced-comment': ['warning', 'always', { 'markers': ['!', '?', 'TODO'] }],
   },
 };
