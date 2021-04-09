db3b2ab1b401a6a79a557041a6ddea2ced879575
Amine <amine1u1@outlook.com>
2021-04-09 22:25:26 +0200

    :label: Use Validators and API decorators on AddBookOwnership DTO
    Now uses class validator to convert dates beforehand, and indicate optional API parameters

--- src/book-ownerships/dto/add-book-ownership.dto.ts
+++ src/book-ownerships/dto/add-book-ownership.dto.ts
@@ -1,10 +1,30 @@
+import { IsDate, IsNotEmpty, IsOptional } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+
 export class AddBookOwnershipDto {
+  @ApiProperty()
+  @IsNotEmpty()
   public bookId: string;
 
+  @ApiPropertyOptional()
   public bought?: boolean;
+
+  @ApiPropertyOptional()
   public signed?: boolean;
+
+  @ApiPropertyOptional()
   public lent?: boolean;
 
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
   public dateAcquired?: Date;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
   public dateRead?: Date;
 }
