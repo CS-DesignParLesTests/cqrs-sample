02695db55eb4c8c4f2febea906a6457de1f3cc2c
Amine <amine1u1@outlook.com>
2021-04-10 13:14:19 +0200

    :label: Omit bookId on UpdateBookOwnershipDto

--- src/book-ownerships/dto/update-book-ownership.dto.ts
+++ src/book-ownerships/dto/update-book-ownership.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookOwnershipDto } from './create-book-ownership.dto';
+import { OmitType } from '@nestjs/swagger';
 
-export class UpdateBookOwnershipDto extends PartialType(CreateBookOwnershipDto) {}
+export class UpdateBookOwnershipDto extends PartialType(
+  OmitType(CreateBookOwnershipDto, ['bookId'] as const),
+) {}
