e5f663d8ec1f841b0cd69fb6b1217458b6a0b4b9
Candice <candylol17@gmail.com>
2021-04-10 13:44:10 +0200

    :sparkles: Add user update command, :label: Add types annotation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
+  @IsNotEmpty()
   username: string;
 
   @ApiPropertyOptional()
+  @IsOptional()
   displayName?: string;
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(OmitType(CreateUserDto, ['username'] as const)) {
+  @IsString()
+  displayName?: string;
+}
