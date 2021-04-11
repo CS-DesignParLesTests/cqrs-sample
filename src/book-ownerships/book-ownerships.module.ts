7e489a1cf76a0c7d742056399c970246bb275f34
Amine <amine1u1@outlook.com>
2021-04-11 05:23:40 +0200

    :building_construction: Make UsersService & BooksService available to BookOwnerShipModule

--- src/book-ownerships/book-ownerships.module.ts
+++ src/book-ownerships/book-ownerships.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { BooksModule } from '../books/books.module';
+import { UsersModule } from '../users/users.module';
+import { BooksService } from '../books/books.service';
+import { UsersService } from '../users/users.service';
 import { BookOwnershipsService } from './book-ownerships.service';
 import { BookOwnershipsController } from './book-ownerships.controller';
 import { BookOwnershipsRepository } from './repositories/book-ownerships-repository';
@@ -9,10 +12,12 @@ import { QueryHandlers } from './queries/handlers';
 import { CommandHandlers } from './commands/handlers';
 
 @Module({
-  imports: [BooksModule, CqrsModule],
+  imports: [BooksModule, UsersModule, CqrsModule],
   controllers: [BookOwnershipsController],
   providers: [
     BookOwnershipsService,
+    BooksService,
+    UsersService,
     {
       provide: BookOwnershipsRepository,
       useClass: BookOwnershipsRepositoryMemoryAdapter,
