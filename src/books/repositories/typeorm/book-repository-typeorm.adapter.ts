ddf87d8f34acc9175fdbcd1b8c268a60582754e3
JeromePto <petot.jerome@gmail.com>
2021-04-11 16:47:41 +0200

    mongo model injection in book repository + on start of the app, sync mongo book db with mysql book db

--- src/books/repositories/typeorm/book-repository-typeorm.adapter.ts
+++ src/books/repositories/typeorm/book-repository-typeorm.adapter.ts
@@ -1,18 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateBookDto } from '../../dto/create-book.dto';
 import { UpdateBookDto } from 'src/books/dto/update-book.dto';
 import { BooksRepository } from '../book-repository';
 import { Book } from '../../entities/book.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { BookMongo, BookMongoDocument } from '../../schemas/book.schema';
 
 @Injectable()
-export class BookRepositoryTypeOrmAdapter extends BooksRepository {
+export class BookRepositoryTypeOrmAdapter
+  extends BooksRepository
+  implements OnApplicationBootstrap {
   constructor(
     @InjectRepository(Book)
     private booksRepository: Repository<Book>,
+    @InjectModel(BookMongo.name) private bookModel: Model<BookMongoDocument>,
   ) {
     super();
+  }
+
+  async onApplicationBootstrap() {
+    console.log('OnApplicationBootstrap from BookRepositoryTypeOrmAdapter : Sync mongo with mysql');
+    const allBooks: Book[] = await this.booksRepository.find();
+    await this.bookModel.deleteMany({});
+    for (let index = 0; index < allBooks.length; index++) {
+      const element: Book = allBooks[index];
+      const createdBook: BookMongoDocument = new this.bookModel(element);
+      await createdBook.save();
+    }
   }
 
   async findOneById(id: string): Promise<Book> {
