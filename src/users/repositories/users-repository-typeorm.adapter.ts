40e49ed2d9e8a22f046d225cc555f786781748ff
JeromePto <petot.jerome@gmail.com>
2021-04-11 18:08:28 +0200

    sync mongo with mysql for users

--- src/users/repositories/users-repository-typeorm.adapter.ts
+++ src/users/repositories/users-repository-typeorm.adapter.ts
@@ -1,17 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UsersRepository } from './users-repository';
 import { User } from '../entities/user.entity';
 import { UpdateUserDto } from '../dto/update-user.dto';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { UserMongo, UserMongoDocument } from '../schemas/user.schema';
 
 @Injectable()
-export class UsersRepositoryTypeOrmAdapter extends UsersRepository {
+export class UsersRepositoryTypeOrmAdapter
+  extends UsersRepository
+  implements OnApplicationBootstrap {
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
+    @InjectModel(UserMongo.name) private userModel: Model<UserMongoDocument>,
   ) {
     super();
+  }
+
+  async onApplicationBootstrap() {
+    console.log(
+      'OnApplicationBootstrap from UsersRepositoryTypeOrmAdapter : Sync mongo with mysql',
+    );
+    const allUsers: User[] = await this.usersRepository.find();
+    await this.userModel.deleteMany({});
+    for (let index = 0; index < allUsers.length; index++) {
+      const element: User = allUsers[index];
+      const createdBook: UserMongoDocument = new this.userModel(element);
+      await createdBook.save();
+    }
   }
 
   async findOneByUsername(username: string): Promise<User | undefined> {
