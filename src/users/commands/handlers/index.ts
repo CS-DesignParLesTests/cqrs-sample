d7756c06c9230fe4143de17452e5389c4df37fa8
JeromePto <petot.jerome@gmail.com>
2021-04-11 18:43:43 +0200

    add missing update in index of handlers for user

--- src/users/commands/handlers/index.ts
+++ src/users/commands/handlers/index.ts
@@ -1,4 +1,5 @@
 import { CreateUserHandler } from './create-user.handler';
 import { DeleteUserHandler } from './delete-user.handler';
+import { UpdateUserHandler } from './update-user.handler';
 
-export const CommandHandlers = [CreateUserHandler, DeleteUserHandler];
+export const CommandHandlers = [CreateUserHandler, DeleteUserHandler, UpdateUserHandler];
