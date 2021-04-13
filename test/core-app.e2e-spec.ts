e8e11259023534c4c25e8e39ada48238853d5243
Amine <amine1u1@outlook.com>
2021-04-13 02:37:15 +0200

    :white_check_mark: Fix and update end to end tests
    Also migrated some controller tests to e2e tests
    
    Co-Authored-By: Maxime Tran <33542706+MaxmeTrn@users.noreply.github.com>

--- test/core-app.e2e-spec.ts
+++ test/core-app.e2e-spec.ts
@@ -5,6 +5,7 @@ import { Connection } from 'typeorm';
 import { AppModule } from '../src/app.module';
 import { Book } from '../src/books/entities/book.entity';
 import { BookOwnership } from '../src/book-ownerships/entities/book-ownership.entity';
+import { User } from '../src/users/entities/user.entity';
 
 describe('Core app', () => {
   let app: INestApplication;
@@ -21,62 +22,86 @@ describe('Core app', () => {
   });
 
   describe('Books', () => {
-    it('Post a book, get by id, delete', async () => {
+    it('Post a book, check returned data, query it by id, update it and finally delete it', async () => {
       const book = {
-        id: 'bc5722e6-2d22-5641-a8a4-4ffe90ce8ebb',
         title: 'Strike The Blood',
         author: 'Gakuto, Mikumo',
       };
 
-      // Post a book
-      const data = await request(app.getHttpServer()).post('/books').send(book).expect(201);
-      expect(data.body).toEqual({
-        ...book,
-      });
+      // POST a book
+      const { body: createdBook } = await request(app.getHttpServer())
+        .post('/books')
+        .send(book)
+        .expect(201);
+      //Check return value
+      expect(createdBook).toMatchObject(book);
+      const bookId = createdBook.id;
 
-      // Get by id
-      const retrievedBook = await request(app.getHttpServer()).get(`/books/${book.id}`).expect(200);
-      expect(retrievedBook.body).toEqual(data.body);
+      // GET one book by id
+      const retrievedBook = await request(app.getHttpServer()).get(`/books/${bookId}`).expect(200);
+      //Check return value
+      expect(retrievedBook.body).toEqual(createdBook);
 
-      // Delete posted book
-      return request(app.getHttpServer()).delete(`/books/${data.body.id}`).expect(200);
-    });
+      // GET the books list
+      const retrievedBookList = await request(app.getHttpServer()).get(`/books`).expect(200);
+      //Check return value
+      expect(retrievedBookList.body).toEqual([createdBook]);
 
-    afterEach(async () => {
-      await connection.createQueryBuilder().delete().from(BookOwnership).execute();
+      //PATCH a book
 
-      await connection.createQueryBuilder().delete().from(Book).execute();
+      await request(app.getHttpServer())
+        .patch(`/books/${createdBook.id}`)
+        .send({ title: 'Strike The Blood !' })
+        .expect(200);
+
+      // GET the updated book by id
+      const retrievedUpdatedBook = await request(app.getHttpServer())
+        .get(`/books/${bookId}`)
+        .expect(200);
+      expect(retrievedUpdatedBook.body).toEqual(
+        Object.assign({}, createdBook, { title: 'Strike The Blood' }),
+      );
+
+      // DELETE posted book
+      await request(app.getHttpServer()).delete(`/books/${createdBook.id}`).expect(200);
     });
   });
 
-  /*
   describe('Users', () => {
-    it('should return an array of users from current test pre-population', async () => {
-      // Pre-populate DB with some dummy books
-      await connection
-        .createQueryBuilder()
-        .insert()
-        .into(User)
-        .values([
-          {
-            username: 'Nathalie',
-            displayName: 'Majora',
-          },
-          {
-            username: 'Benoit',
-            displayName: 'Sepe',
-          },
-        ])
-        .execute();
+    it('post users, get array, update, delete ', async () => {
+      // populate DB with some dummy users
+      const user1 = {
+        username: 'Nathalie',
+        displayName: 'Majora',
+      };
+      const user2 = {
+        username: 'Benoit',
+        displayName: 'Sepe',
+      };
 
-      // Run e2e test
-      const { body } = await request(app.getHttpServer())
-        .get('/books')
-        .set('Accept', 'application/json')
+      // POST both users and check the return values
+
+      const { body: createdUser1 } = await request(app.getHttpServer())
+        .post('/users')
+        .send(user1)
         .expect('Content-Type', /json/)
+        .expect(201);
+      expect(createdUser1).toMatchObject(user1);
+
+      const { body: createdUser2 } = await request(app.getHttpServer())
+        .post('/users')
+        .send(user2)
+        .expect('Content-Type', /json/)
+        .expect(201);
+      expect(createdUser2).toMatchObject(user2);
+
+      //GET Users
+      const { body: users } = await request(app.getHttpServer())
+        .get('/users')
+        .set('Accept', 'application/json')
         .expect(200);
 
-      expect(body).toEqual([
+      expect(users).toEqual([
         {
           username: 'Nathalie',
           displayName: 'Majora',
@@ -86,15 +111,29 @@ describe('Core app', () => {
           displayName: 'Sepe',
         },
       ]);
-    });
 
-    afterEach(async () => {
-      await connection.createQueryBuilder().delete().from(BookOwnership).execute();
+      //PATCH User
+      await request(app.getHttpServer())
+        .patch(`/users/${user2.username}`)
+        .send({ displayName: 'BenoitSepe' })
+        .expect(200);
+      //Check return value
+      const retrievedUpdatedUser = await request(app.getHttpServer())
+        .get(`/users/${user2.username}`)
+        .expect(200);
+      expect(retrievedUpdatedUser.body).toEqual(
+        Object.assign({}, user2, { displayName: 'BenoitSepe' }),
+      );
 
-      await connection.createQueryBuilder().delete().from(User).execute();
+      //DELETE Users
+      await request(app.getHttpServer()).delete(`/users/${user1.username}`).expect(200);
+      await request(app.getHttpServer()).delete(`/users/${user2.username}`).expect(200);
+
+      //Check deletion
+      const { body: finalUsers } = await request(app.getHttpServer()).get(`/users`).expect(200);
+      expect(finalUsers).toEqual([]);
     });
   });
-  */
 
   afterAll(async () => {
     await app.close();
